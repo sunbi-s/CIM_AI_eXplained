364ec7c3177845393134b59deaca222a898fc829
20170375 <sw980907@naver.com>
2023-02-01 19:15:43 +0900

    Add: draw on canvas (WIP)

--- rlboard/rlboard.js
+++ rlboard/rlboard.js
@@ -1,62 +1,70 @@
 class Node{
-    constructor(edge, reward, done){
+    constructor(position, edge, reward, done){
+        this.position = position;
         this.edge = edge;
         this.reward = reward;
         this.done = done;
     }
 
-    draw (){
-        // 길 및 환경 그림
+    draw(canvas) {
+        // draw node
+        canvas.fillStyle = 'blue'
+        canvas.fillRect(this.position[0] * 30, this.position[1] * 30, 30, 30);
+
+        // draw path
+        this.edge.forEach(e => {
+            console.log("draw path", e);
+        });
     }
 }
 
 
 class Environment{
-    constructor(config, agent){
-        this.agent = agent;
+    constructor(config){
+        this.nodes = []
         this._make_board(config);
     }
 
     _make_board(config) {
-        this.nodes = []
-        for (var i=0; i<config.count; ++i) {
-            _edge, _reward, _done = config.nodes[i];
-            _node = Node(_edge, _reward, _done);
-            this.nodes.push(_node);
+        for (let i=0; i<config.nodes.length; ++i) {
+            let _position, _edge, _reward, _done;
+            [_position, _edge, _reward, _done] = config.nodes[i];
+            this.nodes.push(new Node(_position, _edge, _reward, _done));
         }
     }
 
-    draw() {
+    draw(canvas) {
         this.nodes.forEach(node => {
-            node.draw();
+            node.draw(canvas);
         });
     }
 }
 
 
 class Agent{
-    constructor(postion, policy){
-        this.postion = postion;
+    constructor(position, policy){
+        this.position = position;
     }
 
     move(y, x) {
-        this.postion[0] += y;
-        this.postion[1] += x;
+        this.position[0] += y;
+        this.position[1] += x;
     }
 
     action(i_action) {
         this.policy.action(i_action);
     }
 
-    draw(){
-
+    draw(canvas) {
+        canvas.fillStyle = 'red'
+        canvas.fillRect(this.position[0], this.position[1], 30, 30);
     }
 }
 
 
 class Game{
     constructor(seed, policy){
-        this.agent = Agent((0,0), policy);
+        this.agent = new Agent([0, 0], policy);
         this.episode_rewards = [];
         this.step = 0;
 
@@ -64,11 +72,21 @@ class Game{
     }
 
     _config_make(seed){
-        configs = ? //json으로 부르기
-        config = configs[seed];
-        this.environment = Environment(config);
+        // configs = ? //json으로 부르기
+        // config = configs[seed];
+        let config = {
+            'nodes': [
+                [[2, 2], [[2, 0.1]], 0, false],
+                [[4, 4], [[3, 0.3]], 0, false],
+                [[4, 2], [[4, 0.4]], 0, false],
+                [[2, 4], [[5, 0.7]], 0, false],
+                [[6, 4], [[6, 0.2]], 0, false],
+                [[6, 2], [[7, 0.1]], 0, false]
+            ],
+        };
+        this.environment = new Environment(config);
         this.values = [];
-        for (var i=0; i<this.environment.nodes.count; ++i) {
+        for (let i=0; i<this.environment.nodes.length; ++i) {
             this.values.push(0);
         }
     }
@@ -81,14 +99,27 @@ class Game{
         this.agent.move(y, x);
     }
     
-    draw() {
-        this.environment.draw();
-        this.agent.draw();
+    draw(canvas) {
+        // fill background black for debugging
+        canvas.beginPath();
+        canvas.fillStyle = 'black'
+        canvas.fillRect(0, 0, canvas.width, canvas.height);
+
+        this.environment.draw(canvas);
+        this.agent.draw(canvas);
     }
 
-    update() {
-        this.draw();
+    update(canvas) {
+        this.draw(canvas);
     }
 }
 
-    
+
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+c.width = canvas.width;
+c.height = canvas.height;
+
+let game = new Game(0, null)
+game.update(c)
