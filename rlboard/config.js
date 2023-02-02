34fc29eee95c218919c7249d9ca4dec547220c49
20170375 <sw980907@naver.com>
2023-02-02 18:37:59 +0900

    Update: `rlboard.js`

--- rlboard/config.js
+++ rlboard/config.js
@@ -1,23 +1,16 @@
 export default [
     {
         "nodes": [
-            [[5, 1],  false],
-            [[3, 1],  false],
-            [[3, 4],  false],
-            [[1, 5],  false],
-            [[1, 7],  false],
-            [[3, 7],  false],
-            [[6, 7],  true],
+            // [position, reward, final]
+            [[5, 1], 1, false],
+            [[3, 1], 1, false],
+            [[3, 4], 1, false],
+            [[1, 5], 1, false],
+            [[1, 7], 1, false],
+            [[3, 7], 1, false],
+            [[6, 7], 100, true],
         ],
-        "graph": [
-            [1],
-            [2],
-            [1,3],
-            [2,4,6],
-            [3,5],
-            [4],
-            [3]
-        ],
-        "agentStartIdx": 1
+        // [x, y]
+        "agentStartPosition": [0, 0]
     },
 ];
