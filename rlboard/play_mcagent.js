c7e54426f5f4230f60b69129272e2e53a9556cda
20170375 <62216628+20170375@users.noreply.github.com>
2023-02-07 22:07:20 +0900

    Add: 'select' DOM for max_episode_num in 'Game.run()' method

--- rlboard/play_mcagent.js
+++ rlboard/play_mcagent.js
@@ -14,6 +14,7 @@ c2.height = canvas_2.height;
 let policyName = "mc";
 let game = new Game(c, c2, 0, policyName);
 
+let selectNum = frame.querySelector('.select_num');
 let btnTrain = frame.querySelector('.btn_train');
 let btnTest = frame.querySelector('.btn_test');
 let btnReset = frame.querySelector('.btn_reset');
@@ -21,7 +22,7 @@ let btnReset = frame.querySelector('.btn_reset');
 btnTrain.addEventListener("click", function() {
     btnTrain.disabled = true;
     btnTest.disabled = true;
-    game.run(10).then(() => {
+    game.run(selectNum.value).then(() => {
         btnTrain.disabled = false;
         btnTest.disabled = false;
     });
