4feba2e37e92674ad7de2308b80242e1f112cf67
sunbi-s <is6978@naver.com>
2023-02-15 20:42:14 +0900

    layout

--- rlboard/3D_table.js
+++ rlboard/3D_table.js
@@ -12,8 +12,33 @@ function getData() {
 console.log(getData());
 
 
-Plotly.newPlot( TESTER, [{
-  z: getData(),
-  type: 'surface'
+var layout = {
+    title: 'test value',
+    scene: {camera: {eye: {x: 1.87, y: 0.88, z: 1.64}}},
+    autosize: true,
+    width: 1000,
+    height: 800,
+    font: {
+        size: 15,
+    },
+    margin: {
+      l: 40,
+      r: 0,
+      b: 20,
+      t: 0,
+    },
+    scene:{
+        zaxis:{
+            title: "Value"
+        }
+    }
+  };
 
-}]);
+
+var data = [{
+    z: getData(),
+    type: 'surface'
+      
+  }]
+
+Plotly.newPlot( TESTER, data, layout);
