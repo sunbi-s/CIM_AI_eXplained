989210ffde398d05aadc69761fa5697d158debd3
20170375 <sw980907@naver.com>
2023-02-18 00:06:04 +0900

    Update: arrow images

--- rlboard/play_trajectory.js
+++ rlboard/play_trajectory.js
@@ -1,11 +1,11 @@
 import { Position, Game, animate } from "./rlboard.js";
 
 const frame = document.querySelector('#play_6')
-const board = frame.querySelector('.board');
-board.style.cursor = 'pointer';
+const boardDom = frame.querySelector('.board');
+boardDom.style.cursor = 'pointer';
 
 let policyName = "control";
-let game = new Game(board, 0, policyName);
+let game = new Game(boardDom, 0, policyName);
 animate(game);
 
 let state = game.environment.reset();
@@ -27,22 +27,31 @@ function getCellPosition(div, _cell) {
     }
 }
 
+function setDraggable(node) {
+    node.classList.add("draggable");
+    node.addEventListener("dragstart", () => {
+        node.classList.add("dragging");
+    })
+    node.addEventListener("dragend", () => {
+        node.classList.remove("dragging");
+    });
+}
 
 function make_action_table(){
     
-    arrows = board.querySelectorAll(".arrow");
+    let arrows = board.querySelectorAll(".arrow");
     
-    arrows.forEach((arrow) =>{
+    arrows.forEach((arrow) => {
         //posiotin
-        cell = arrow.parentNode
-        position = getCellPosition(cell)
+        let cell = arrow.parentNode;
+        let position = getCellPosition(cell);
         
-        x = position[0]
-        y = position[1]
+        x = position[0];
+        y = position[1];
 
         //direction  this.actions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-        directions = ['up','down','left','right']
-        for(var i = 1; i <4; i++) {
+        let directions = ['up','down','left','right']
+        for(let i = 1; i < 4; ++i) {
             if (arrow.contains(directions[i])){
                 
             }
@@ -51,76 +60,52 @@ function make_action_table(){
 
 }
 
-//player
-const playerDom = game.environment.player.dom;
-playerDom.style.position = "absolute"
 
 // 다른 공간에서 화살표를 가져왔다고 생각하고 추가.
 let tempdiv = document.createElement('img');
-tempdiv.src = "../img/rlboard/arrow/up.png"
-tempdiv.classList.add(".arrow")
-tempdiv.classList.add(".up")
-tempdiv.classList.add("draggable")
-//////////////////////////////////////////////////
+tempdiv.src = "../img/rlboard/arrow/up.png";
+tempdiv.classList.add(".arrow");
+tempdiv.classList.add(".up");
+setDraggable(tempdiv);
 
-let x = 3
-let y = 0
+let y = 0;
+let x = 3;
 
-board.querySelectorAll(".cell")[10*y + x].appendChild(tempdiv)
-////////////////////////
+boardDom.querySelectorAll(".cell")[10*y + x].appendChild(tempdiv)
 
-function getCellPosition(div, _cell) {
-    for (let y=0; y<div.childElementCount; ++y) {
-        let row = div.childNodes[y];
-        for (let x=0; x<row.childElementCount; ++x) {
-            let cell = row.childNodes[x];
-            if (cell === _cell) {
-                return new Position(y, x);
-            }
-        }
-    }
-}
-
-// drag 중인 객체 구분
-const draggables = board.querySelectorAll(".draggable");
-draggables.forEach((draggable) => {
-    draggable.addEventListener("dragstart", () => {
-        console.log(draggable)
-        draggable.classList.add("dragging");
-    })
-    draggable.addEventListener("dragend", () => {
-        draggable.classList.remove("dragging");
-    });
-});
 
 // drag 중인 객체 이동
-let cells = board.querySelectorAll(".cell");
+const cells = boardDom.querySelectorAll(".cell");
 cells.forEach((cell) => {
     cell.addEventListener("dragover", (e) => {
         e.preventDefault();
-        const draggable = board.querySelector(".dragging");
+    });
+    cell.addEventListener("drop", (e) => {
+        e.preventDefault();
+        let draggable = boardDom.querySelector(".dragging");
+        if (draggable == null) {
+            return;
+        }
+
         if (cell.childElementCount === 0) {
-            cell.appendChild(draggable);
-        } else if (draggable.classList.contains("player")) {
-            // cell.insertBefore(draggable, cell.firstChild);
             cell.appendChild(draggable);
         }
     });
 });
 
+
+// Add btn event
 let btnTest = frame.querySelector('.btn_test');
 
 btnTest.addEventListener("click", function() {
     btnTest.disabled = true;
-    
-    let state,  
-    let done = false
 
-    while(!done){
-        = environment.reset()
+    state = game.environment.reset();
+    while (!done) {
+        done = game.environment.step(0);
+        done = true;
     }
     game.run_test(1).then(() => {
-        btnTrain.disabled = false;
         btnTest.disabled = false;
     });
 });
