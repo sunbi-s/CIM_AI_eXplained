7d804f893576451aff5c6121f1ad3f91b73d56e4
sunbi-s <is6978@naver.com>
2023-03-20 20:28:07 +0900

    optim avg and argmax

--- rlboard/agents.js
+++ rlboard/agents.js
@@ -173,7 +173,7 @@ export class OptimAgent {
         }
 
         // calculate optimal value table
-        for (let i = 0; i < 100; ++i) {
+        for (let i = 0; i < 1000; ++i) {
             this._valueIteration();
         }
     }
@@ -275,4 +275,134 @@ export class OptimAgent {
         }
         return max_index_list[Math.floor(Math.random() * max_index_list.length)];
     }
+
 }
+
+export class OptimAgentAVG{
+    // https://github.com/rlcode/reinforcement-learning/blob/master/1-grid-world/2-value-iteration/value_iteration.py#L4
+    constructor(env) {
+        this.env = env;
+        this.height = env.boardShape[0];
+        this.width = env.boardShape[1];
+        this.actions = env.actions;
+        this.discount_factor = 0.9;
+
+        this.initValueTable();
+    }
+
+    initValueTable() {
+        this.value_table = [];
+        for (let y = 0; y < this.height; ++y) {
+            let row = [];
+            for (let x = 0; x < this.width; ++x) {
+                row.push(0);
+            }
+            this.value_table.push(row);
+        }
+
+        // calculate optimal value table
+        for (let i = 0; i < 1000; ++i) {
+            this._valueIteration();
+        }
+    }
+
+    _valueIteration() {
+        let next_value_table = [];
+        for (let y = 0; y < this.height; ++y) {
+            let row = [];
+            for (let x = 0; x < this.width; ++x) {
+                row.push(0);
+            }
+            next_value_table.push(row);
+        }
+
+        for (let y = 0; y < this.height; ++y) {
+            for (let x = 0; x < this.width; ++x) {
+                let state = [y, x];
+                let cell = this.env._getCell(new Position(state[0], state[1]));
+                if (cell) {
+                    let place = cell.lastChild;
+                    if (place && place.classList.contains("place")) {
+                        if (place.done) {
+                            next_value_table[state[0]][state[1]] = 0;
+                            continue;
+                        }
+                    }
+                }
+
+                let value_list = [];
+
+                for (let action of this.actions) {
+                    let next_state = [state[0], state[1]];
+                    if (next_state[0] + action[0] < 0 || next_state[0] + action[0] > this.height - 1){
+                        continue;
+                    }
+                    if (next_state[1] + action[1] < 0 || next_state[1] + action[1] > this.width - 1){
+                        continue;
+                    }
+
+                    next_state[0] = next_state[0] + action[0]
+                    next_state[1] = next_state[1] + action[1]
+
+                    let reward = -1;
+                    let next_cell = this.env._getCell(new Position(next_state[0], next_state[1]));
+                    if (next_cell) {
+                        let place = next_cell.lastChild;
+                        if (place && place.classList.contains("place")) {
+                            reward += place.reward;
+                        }
+
+                        let next_value = this.value_table[next_state[0]][next_state[1]];
+                        value_list.push(reward + this.discount_factor * next_value);
+                    }
+                }
+                let sum = value_list.reduce((a, b) => a + b, 0);
+                next_value_table[state[0]][state[1]] = sum / value_list.length;
+                // next_value_table[state[0]][state[1]] = Math.max(...value_list);
+            }
+        }
+        this.value_table = next_value_table;
+    }
+
+    getOptimalAction(state) {
+        // Action based on Q-value
+        let value_list = [];
+        for (let action of this.actions) {
+            let next_state = [state[0], state[1]];
+            next_state[0] = clamp(next_state[0] + action[0], 0, this.height - 1);
+            next_state[1] = clamp(next_state[1] + action[1], 0, this.width - 1);
+
+            let reward = -1;
+            let next_cell = this.env._getCell(new Position(next_state[0], next_state[1]));
+            if (next_cell) {
+                let place = next_cell.lastChild;
+                if (place && place.classList.contains("place")) {
+                    reward += place.reward;
+                }
+                let next_value = this.value_table[next_state[0]][next_state[1]];
+                console.log(reward, state[0], state[1], next_state[0], next_state[1], next_value)
+                value_list.push(reward + this.discount_factor * next_value);
+            }
+        }
+        return this._argMax(value_list);
+    }
+
+    // Calculate arg_max if there are multiple candidates and return one randomly
+    _argMax(next_state) {
+        let max_index_list = [];
+        let max_value = next_state[0];
+        for (let i = 0; i < next_state.length; i++) {
+            let value = next_state[i];
+            if (value > max_value) {
+                max_index_list.length = 0;
+                max_value = value;
+                max_index_list.push(i);
+            } else if (value === max_value) {
+                max_index_list.push(i);
+            }
+        }
+        return max_index_list[Math.floor(Math.random() * max_index_list.length)];
+    }
+
+}
+
